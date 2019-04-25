ecf71f393aa898119560c54ccda09fd8b1dcaf5b
Will Sutton <wsutton17@gmail.com>
2019-04-25 10:41:37 -0400

    adding more data-build + fit utils

--- modules/ap3data.py
+++ modules/ap3data.py
@@ -4,26 +4,34 @@ import numpy as np
 from fastai.vision import *
 from fastai.utils.mem import  gpu_mem_get_free_no_cache
 
-# gpu_mem_get_free_no_cache()
-
 ### setup data building environment -------------------------------------
 
-data_dir = Path('data/alphapilot/')
-raw_fn = data_dir/'data_training'
-label_fn = data_dir/'truth_df.csv'
 
-if os.name == 'nt':      ##local
-    # data_dir = Path('../../../../alphapilot/')
-    data_dir = Path('c:/users/wsutt/desktop/files/alphapilot/')  # absolute
+def is_local(name='DESKTOP-5VTC260'):
+    sys_vars = os.environ
+    return ( sys_vars.get('NAME', '') == name
+             or sys_vars.get('COMPUTERNAME', '') == name)
+
+b_local = is_local()
+b_windows = (os.name=='nt')
+
+if b_windows:      
+    data_dir = Path('c:/users/wsutt/desktop/files/alphapilot/')
     raw_fn = data_dir/'Data_Training/Data_Training/'
     label_fn = data_dir/'truth_df.csv'
+elif b_local:
+    data_dir = Path('/mnt/c/users/wsutt/desktop/files/alphapilot/')
+    raw_fn = data_dir/'Data_Training/Data_Training/'
+    label_fn = data_dir/'truth_df.csv'
+else:
+    data_dir = Path('data/alphapilot/')
+    raw_fn = data_dir/'data_training'
+    label_fn = data_dir/'truth_df.csv'
 
-#load ground truth
 truth_df = pd.read_csv(label_fn, index_col=0)
 assert truth_df.shape == (8, 5827)
 
 TRUTH_INDS  = list(truth_df.columns)
-
 
 #utility functions -----
 def filter_img_by_truth(fn):
@@ -91,22 +99,36 @@ def avg_prediction(dataset, as_int=False, as_flat=False):
 
     return avg_y_flat
 
-
+def clear_gpu_mem(learner=None):
+    ''' for consecutive model building in a notebook '''
+    print(gpu_mem_get_free_no_cache())
+    try:
+        learner.purge()
+        learner.destroy()
+    except:
+        print('failed to purge/destroy the learner')
+    print(gpu_mem_get_free_no_cache())
 
 def build_data(
                 batch_size = None,
                 size = None,
                 num_workers = None,
-                bypass_validation = True,
+                bypass_validation = False,
                 seed = None,
+                valid_pct = None,
                 mini_data = False,
                 ):
+    ''' reporducible, parameterized module for returning DataBunch '''
 
     _numworkers = {}
-    if os.name == 'nt':
-        _numworkers['num_workers'] = 0
     if num_workers is not None:
-        _numworkers['num_workers'] = num_workers
+        _numworkers['num_workers'] = numworkers
+    elif b_windows:
+        _numworkers['num_workers'] = 0
+    elif b_local and not(b_windows):
+        _numworkers['num_workers'] = 4
+    else:
+        _numworkers['num_workers'] = 8
         
     _batchsize = 4
     if batch_size is not None:
@@ -120,7 +142,12 @@ def build_data(
     if seed is not None:
         _seed = seed
 
-    np.random.seed(_seed)  # called each time you eneter function
+    _valid_pct = 0.2
+    if valid_pct is not None:
+        _valid_pct = valid_pct
+
+    # called each time you eneter function
+    np.random.seed(_seed)  
 
     filter_records = filter_img_by_truth
     if mini_data:
@@ -128,7 +155,7 @@ def build_data(
 
     data = (PointsItemList.from_folder(raw_fn)
             .filter_by_func(filter_records)
-            .split_by_rand_pct(valid_pct=0.2)
+            .split_by_rand_pct(valid_pct=_valid_pct)
             .label_from_func(label_points)
             .transform(get_transforms()
                                 ,tfm_y=True
@@ -142,15 +169,18 @@ def build_data(
     if bypass_validation:
         return data
     
-    assert isinstance(data, ImageDataBunch)
-    assert len(data.train_dl.x.items) == 4662
-    assert len(data.valid_dl.x.items) == 1165
-    assert list(data.valid_dl.y.items[0].shape) == [4,2]
-    assert list(data.valid_dl.x.get(0).shape) == [3, 864, 1296]
-    assert data.num_workers == (0 if os.name == 'nt' else 8)
-    assert str(data.path) == ( 'data/alphapilot/data_training'
-                            if os.name != 'nt' else
-                            '..\\..\\..\\..\\alphapilot\\Data_Training\\Data_Training'
-                                )
+    try:
+        assert isinstance(data, ImageDataBunch)
+        assert len(data.train_dl.x.items) == 4662
+        assert len(data.valid_dl.x.items) == 1165
+        assert list(data.valid_dl.y.items[0].shape) == [4,2]
+        assert list(data.valid_dl.x.get(0).shape) == [3, 864, 1296]
+        assert data.num_workers == (0 if os.name == 'nt' else 8)
+        assert str(data.path) == ( 'data/alphapilot/data_training'
+                                if not(b_local) else
+                                '..\\..\\..\\..\\alphapilot\\Data_Training\\Data_Training'
+                                    )
+    except Exception as e:
+        print(e)
 
     return data
